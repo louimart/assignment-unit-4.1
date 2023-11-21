94a674fc2fd48235782b9ac6f6b31a06fed1ddb6
Louis Martin <louimart@gmail.com>
2023-11-21 11:47:37 -0600

    completed stretch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,7 +15,7 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  console.log('>>IN helloName')
+  console.log('>>> IN helloName <<<')
   let personalizedHello = `Hello, ${name}!`;
   // console.log(personalizedHello);
   return personalizedHello;
@@ -26,14 +26,14 @@ console.log(helloName('Louis'));
 // 3. Function to add two numbers together & return the result
 function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
-  console.log('>>IN addNumber');
+  console.log('>>> IN addNumber <<<');
   return firstNumber + secondNumber;
 } // END addNumbers
 console.log(addNumbers(2, 10));
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree(num1, num2, num3) {
-  console.log('>>IN multiplyThree');
+  console.log('>>> IN multiplyThree <<<');
   return num1 * num2 * num3;
 }
 console.log(multiplyThree(2, 3, 4))
@@ -41,7 +41,7 @@ console.log(multiplyThree(2, 3, 4))
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
-  console.log('>>IN isPositive')
+  console.log('>>> IN isPositive <<<')
   if (number > 0) {
     console.log(true, `The value ${number} is positive and greater than zero.`);
     return true;
@@ -61,7 +61,7 @@ isPositive(22);
 //    array is empty, return `undefined`.
 array = [ 1, 2, 3, 5 ]
 function getLast(array) {
-  console.log('>>IN getLast')
+  console.log('>>> IN getLast <<<')
   let list = [ array.pop() ];
   return list.pop(array);
 }
@@ -72,11 +72,12 @@ console.log(getLast(array));
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 array = [ 23, 43, 88, 16, 10, 5 ];
 function find(value, array2) {
-  console.log('>>IN find')
+  console.log('>>> IN find <<<')
   // for (let x=0; x<array.length; x++){
   for (let x of array2){
-    console.log(x);
+    // console.log(x);
     if (x === value){
+      console.log(`${value} was found in array.`)
       return true;
     }
   }
@@ -102,27 +103,33 @@ console.log(isFirstLetter('a', 'apple'));
 console.log(isFirstLetter('l', 'bell'));
 
 // 9. Function to return the sum of all numbers in an array
-// function sumAll(array1) {
-//   let sum = 0;
+newArray = [1, -2, 3, -4, 5]
+function sumAll(array) {
+  console.log('>>> IN sumAll <<<')
+  let sum = 0;
   // TODO: loop to add items
-//   for ( let i = 0 ; i < array1.lenght; i += 1 ){
-//     console.log('IN SUMALL')
-//     sum += array1[i];
-//     console.log(array[i]);
-
-//     // console.log(sum);
-//   }
-//   // TODO: return the sum
-//   return sum;
-// } sumAll(array);
+  for ( let i of array ){
+    sum += i;
+  }
+  // TODO: return the sum
+  return sum;
+} 
+console.log(`The sum of the array is ${sumAll(newArray)}.`);
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-function allPositive() {
-
+function allPositive(array) {
+  console.log('>>> In allPositive <<<')
+  let copyArray = [];
+  for ( let i = 0; i < array.length; i++ ) {
+    if (array[i] > 0){
+      copyArray.push(array[i]);
+  }
+} return copyArray;
 }
-
+console.log(`Numbers in the array are ${newArray}.`);
+console.log(`Positive numbers in the array are ${allPositive(newArray)}.`);
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
